52a656a92df0b6583bd431c63e0af443e987e1d6
dakingha69 <dongha.kim210@gmail.com>
2017-06-29 17:20:07 +0200

    Add public methods for creating and loading users in TLNetwork class

--- src/TLNetwork.ts
+++ src/TLNetwork.ts
@@ -1,10 +1,34 @@
 import { Configuration } from './Configuration'
+import { User } from './User'
 
 export class TLNetwork {
     private configuration: Configuration
+    private user: User
 
     constructor(config: any = {}) {
         const { host, port, tokenAddress, pollInterval, useWebSockets } = config
         this.configuration = new Configuration(host, port, tokenAddress, pollInterval, useWebSockets)
+        this.user = new User()
+    }
+
+    public createUser(username: string) {
+        this.user.username = username
+        return new Promise((resolve, reject) => {
+            this.user.generateKey().then((address) => {
+                this.user.address = address
+                const createdUser = {
+                    username: this.user.username,
+                    address: this.user.address,
+                    keystore: this.user.keystore
+                }
+                resolve(createdUser)
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    }
+
+    public loadUser(keystore: string) {
+        this.user.keystore = keystore
     }
 }
