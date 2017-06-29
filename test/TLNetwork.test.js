b47ccccdee1b8fff1324edf64cc386c3cf9644c0
dakingha69 <dongha.kim210@gmail.com>
2017-06-29 17:20:44 +0200

    Add test cases for user class

--- test/TLNetwork.test.js
+++ test/TLNetwork.test.js
@@ -1,10 +1,18 @@
 var chai = require('chai');
 var expect = chai.expect;
-var TLNetwork = require('../dist/TLNetwork.js').TLNetwork;
+var TLNetwork = require('../dist/trustlines-network.js').TLNetwork;
 
 describe('TLNetwork Class', () => {
-    it('TLNetwork can be constructed', () => {
-        var tl = new TLNetwork()
-        expect(tl).to.be.an('object')
+    var tlNetwork = new TLNetwork()
+
+    it('TLNetwork can be initiated', () => {
+        expect(tlNetwork).to.be.an('object')
+    })
+
+    it('createUser creates new user', () => {
+        return tlNetwork.createUser().then((createdUser) => {
+            console.log(createdUser)
+            expect(createdUser).to.be.an('object')
+        })
     })
 })
