4697229539b6ac0bd36f749d3eea2941bcdb07c1
dakingha69 <dongha.kim210@gmail.com>
2017-07-04 10:49:58 +0200

    Change private attributes of Configuration class to public

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -1,20 +1,20 @@
 export class Configuration {
     // url of rest api
-    private apiUrl: string
+    public apiUrl: string
     // url of websocket api
-    private wsApiUrl: string
+    public wsApiUrl: string
 
     constructor(
         // host of the REST relay server
-        private host: string = "localhost",
+        public host: string = "localhost",
         // port of REST relay server
-        private port: number = 5000,
+        public port: number = 5000,
         // address of the token
-        private tokenAddress: string = "0x61f3a6deebc44c0421f5d8e5a34cf5f21e862b39",
+        public tokenAddress: string = "0x61f3a6deebc44c0421f5d8e5a34cf5f21e862b39",
         // poll interval
-        private pollInterval: number = 500,
+        public pollInterval: number = 500,
         // use websockets?
-        private useWebSockets: boolean = false
+        public useWebSockets: boolean = false
     ) {
         this.apiUrl = `http://${this.host}:${this.port}/api/`
         this.wsApiUrl = `ws://${this.host}:${this.port}/api/`
