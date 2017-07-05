916f0625a01ce407c1d74402552a0f7129043c96
dakingha69 <dongha.kim210@gmail.com>
2017-07-05 17:19:54 +0200

    Add currency network class

---
+++ src/CurrencyNetwork.ts
@@ -0,0 +1,34 @@
+import { TLNetwork } from "./TLNetwork"
+
+import "isomorphic-fetch"
+import { polyfill } from "es6-promise"
+
+export class CurrencyNetwork {
+
+    constructor(private tlNetwork: TLNetwork) {}
+
+    public getNetworks(): Promise<string[]> {
+        const { configuration, user } = this.tlNetwork
+        // TODO define relay api
+        return fetch(`${configuration.apiUrl}tokens/users/${user.address}`)
+            .then(res => res.json())
+            .then(json => json.tokens)
+            .catch(e => console.error(e))
+    }
+
+    public getAllNetworks(): Promise<string[]> {
+        const { configuration } = this.tlNetwork
+        return fetch(`${configuration.apiUrl}tokens`)
+            .then(res => res.json())
+            .then(json => json.tokens)
+            .catch(e => console.error(e))
+    }
+
+    public getNetworkInfo(address: string): Promise<any> {
+        const { configuration } = this.tlNetwork
+        return fetch(`${configuration.apiUrl}tokens/${address}`)
+            .then(res => res.json())
+            .catch(e => console.error(e))
+    }
+
+}
