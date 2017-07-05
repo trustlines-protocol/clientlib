213e1e5de8ce82684bde820e56e64aff92542f77
dakingha69 <dongha.kim210@gmail.com>
2017-07-05 17:20:36 +0200

    Add Trustline class

---
+++ src/Trustline.ts
@@ -0,0 +1,40 @@
+import { ObservableHelper } from "./ObservableHelper"
+import { TLNetwork } from "./TLNetwork"
+
+import "isomorphic-fetch"
+import { polyfill } from "es6-promise"
+
+export class Trustline {
+
+    constructor(private tlNetwork: TLNetwork) {}
+
+    public update(debtor: string, value: number): Promise<string> {
+        const { transaction, user } = this.tlNetwork
+        return transaction.prepareTransaction("updateCreditline", ["0x" + debtor, value])
+            .then((tx) => user.signTx(tx))
+            .then((tx) => transaction.relayTx(tx))
+    }
+
+    // TODO: define smart contract method
+    public accept(creditor: string) {
+        const { transaction, user } = this.tlNetwork
+        return transaction.prepareTransaction("acceptCreditline", ["0x" + creditor])
+            .then((tx) => user.signTx(tx))
+            .then((tx) => transaction.relayTx(tx))
+    }
+
+    // TODO: define smart contract method
+    public decline(creditor: string) {
+        const { transaction, user } = this.tlNetwork
+        return transaction.prepareTransaction("declineCreditline", ["0x" + creditor])
+            .then((tx) => user.signTx(tx))
+            .then((tx) => transaction.relayTx(tx))
+    }
+
+    public getAll(): Promise<string[]> {
+        const { configuration, transaction, user, defaultNetwork } = this.tlNetwork
+        return fetch(`${configuration.apiUrl}tokens/${defaultNetwork}users/0x${user.address}accounts/`)
+          .then(res => res.json())
+          .then(json => json.accounts)
+    }
+}
