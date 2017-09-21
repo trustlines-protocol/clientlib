b886668a00402e88d26d35ccd2704ac997b9ba32
Dong-Ha Kim <dongha.kim210@gmail.com>
2017-09-21 13:05:38 +0200

    Enhance fixtures

--- tests/Fixtures.ts
+++ tests/Fixtures.ts
@@ -3,8 +3,23 @@ export const keystore1 = `{"encSeed":{"encStr":"60/PJ6+Kuz/o0py2GtN/vQ4gxlBDwahT
 export const keystore2 = `{"encSeed":{"encStr":"UJrWA2ZaPF6lnWVktF/VjtLFJ3EZRpmhaw/wWMxMpNIxllyslNFtPSO4fWCn5v5hXqbjD9cCCWoRFYhKeXUsp2bN4uU6TJbQfGEtpT4ptOmoo4TJKbZNdIYq4ldDh7LidayT49zd+0+Qo4/ZP8Kj4NVQO7BaqkZgUhv41gr68nMWErecZhBXNQ==","nonce":"iJpRWz6rlhWlV0EF/Ig4/nvYmBq1H5II"},"ksData":{"m/44'/60'/0'/0":{"info":{"curve":"secp256k1","purpose":"sign"},"encHdPathPriv":{"encStr":"iXJJHp3vDwQOeBkhcmp6+4B7NGnf4CYxBRRYOk2DxZEz0SSJM0F1woJn1rQi3nRVdxFoUfVXEhUHr3Nkw+4B+jGX0LWjG81DkJ9ovSe/3BMSkXyUwFyZgyOLFinLrMH22uJ7BT0l9h5e9plLTzjsvLk+7q8YrKgylGt/6flFKA==","nonce":"0wLXvs0gppLxHMpOAnX/kQkTnNCL5467"},"hdIndex":1,"encPrivKeys":{"ce2d6f8bc55a61428d32947bc9bc7f2de1640b18":{"key":"t4pBsWrvJx3Zn/msrNAmJuH7btIawqhTUasDl12evPBcG3eKoljEyJF5dG3N5QOK","nonce":"du/KqEumtY9nbxT5Zs23i8zi5AterRTY"}},"addresses":["ce2d6f8bc55a61428d32947bc9bc7f2de1640b18"]},"m/44'/60'/0'/1":{"info":{"curve":"curve25519","purpose":"asymEncrypt"},"encHdPathPriv":{"encStr":"eqNBlf119aaDQRTCWW14OJtvTba7EkaFEw30BuXHvNG61h/0e9qC59sF1TvNqSg2fhWFzWN7Z9WJd5f+Yy6dq6487goJuc/UhI99wlvHhG+p7mIsSfP5QayV1U2XZv+JTHbU3nfRs91dhzIhc4j3lUySbm3pfmEPnqaxoCHe9A==","nonce":"52IHSj2WeHDWyqazIaeBrMnClAyFk4Ut"},"hdIndex":1,"encPrivKeys":{"423b6f895e9eda6dff738c9e572b17dd76db460564b71a9eef864ac89930eb77":{"key":"db4QvXmmb/Rk/xaVHurnAhjEzWn+KNeLo/y92j/ffG3a1slJu27CWsIVMfCKAKNT","nonce":"IHnuBp4VDGl4wR7zJ5ikdG6OqTQFMH4b"}},"pubKeys":["423b6f895e9eda6dff738c9e572b17dd76db460564b71a9eef864ac89930eb77"]}},"encHdRootPriv":{"encStr":"rIUuzH9VunRl3TzfsmDhOqFd8s083i+2rTeRBOTIm6sRVTSlwPmFnpLXLZji60mzGBkUpmcYvV7t10EA/UyT3pBVC+D+9KrWLT9GieTAJIEYryJUBFXr05OSBOziSOcTBp8a1LEFkvrgUuApqLmsHN3j+VDyZIOXsiHqR67ofg==","nonce":"Clkz0qaQ8YIUf8EaicZq8S5AfHz4o2vQ"},"salt":"jICGZMaFF14gIhzmxG/twe/et3UpMhHfH0cYxGPljXg=","version":2}`
 
 export const config = {
-   protocol: 'https',
-   host: "testrelay0.trustlines.network",
-   useWebSockets: false,
-   path: "api/v1/"
- }
+  protocol: 'https',
+  host: "testrelay0.trustlines.network",
+  useWebSockets: false,
+  path: "api/v1/"
+}
+
+export const user1 = {
+  address: '0xf8e191d2cd72ff35cb8f012685a29b31996614ea',
+  proxyAddress: '0xf8e191d2cd72ff35cb8f012685a29b31996614ea',
+  keystore: keystore1,
+  pubKey: 'ac00e064896b56904b2cc7da3ba4a77a12abb0c2d0734620febed448e648e77f'
+}
+
+export const user2 = {
+  address: '0xce2d6f8bc55a61428d32947bc9bc7f2de1640b18',
+  proxyAddress: '0xce2d6f8bc55a61428d32947bc9bc7f2de1640b18',
+  keystore: keystore2
+}
+
+export const networkAddress = '0x2a2b68e9bf718dbf6df3df91174a136054a8a0ec'
