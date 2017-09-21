a4c0260ed395db93ca02b55f11f40bba5fc14739
Dong-Ha Kim <dongha.kim210@gmail.com>
2017-09-21 13:10:18 +0200

    Add tests for Trustlines class

---
+++ tests/Trustline.test.ts
@@ -0,0 +1,93 @@
+import { TLNetwork } from '../src/TLNetwork'
+import { expect } from 'chai'
+import 'mocha'
+import { config, keystore1, keystore2, networkAddress } from './Fixtures'
+
+declare let Promise: any
+
+describe('Trustline', () => {
+
+  const tl1 = new TLNetwork(config)
+  const tl2 = new TLNetwork(config)
+  let user1, user2, rawTx
+
+  it('should prepare creditline update', done => {
+    Promise.all([tl1.user.load(keystore1), tl2.user.load(keystore2)])
+      .then(results => {
+        [ user1, user2 ] = results
+        tl1.trustline.prepareUpdate(networkAddress, user2.proxyAddress, 1300).then(txObj => {
+          rawTx = txObj.rawTx
+          expect(rawTx).to.be.a('string')
+          expect(txObj.ethFee).to.be.a('number')
+          expect(txObj.gasPrice).to.be.a('number')
+          done()
+        })
+      })
+  })
+
+  it('should confirm creditline update', done => {
+    tl1.trustline.confirm(rawTx).then(txId => {
+      expect(txId).to.be.a('string')
+      done()
+    })
+  })
+
+  it('should prepare creditline accept', done => {
+    tl2.trustline.prepareAccept(networkAddress, user1.proxyAddress, 1300).then(txObj => {
+      rawTx = txObj.rawTx
+      expect(rawTx).to.be.a('string')
+      expect(txObj.ethFee).to.be.a('number')
+      expect(txObj.gasPrice).to.be.a('number')
+      done()
+    })
+  })
+
+  it('should confirm creditline accept', done => {
+    tl2.trustline.confirm(rawTx).then(txId => {
+      expect(txId).to.be.a('string')
+      done()
+    })
+  })
+
+  it('should return updated trustline', done => {
+    tl1.trustline.get(networkAddress, user2.proxyAddress).then(trustline => {
+      expect(trustline.given).to.equal(1300)
+      expect(trustline.address).to.equal(user2.proxyAddress)
+      done()
+    })
+  })
+
+  it('should return all trustlines of user', done => {
+    tl1.trustline.getAll(networkAddress).then(trustlines => {
+      expect(trustlines).to.be.an('array')
+      done()
+    })
+  })
+
+  it('should return all creditline requests', done => {
+    tl1.trustline.getRequests(networkAddress).then(requests => {
+      const latestRequest = requests[requests.length - 1]
+      expect(requests).to.be.an('array')
+      expect(latestRequest.networkAddress).to.equal(networkAddress)
+      expect(latestRequest.address).to.equal(user2.proxyAddress)
+      expect(latestRequest.amount).to.equal(1300)
+      expect(latestRequest.direction).to.equal('sent')
+      expect(latestRequest.type).to.equal('CreditlineUpdateRequest')
+      done()
+    })
+  })
+
+  it('should return all creditline updates', done => {
+    tl1.trustline.getUpdates(networkAddress).then(updates => {
+      const latestUpdate = updates[updates.length - 1]
+      expect(updates).to.be.an('array')
+      expect(latestUpdate.networkAddress).to.equal(networkAddress)
+      expect(latestUpdate.address).to.equal(user2.proxyAddress)
+      expect(latestUpdate.amount).to.equal(1300)
+      expect(latestUpdate.direction).to.equal('sent')
+      expect(latestUpdate.type).to.equal('CreditlineUpdate')
+      done()
+    })
+  })
+
+})
