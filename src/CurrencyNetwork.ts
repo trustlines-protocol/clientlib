704f368d166fe3729a0c2745272f69795307eaf4
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-01-10 13:21:59 +0100

    Add getDecimals to CurrencyNetwork

--- src/CurrencyNetwork.ts
+++ src/CurrencyNetwork.ts
@@ -30,4 +30,15 @@ export class CurrencyNetwork {
     return this.utils.fetchUrl(`networks/${networkAddress}/users/${userAddress}`)
   }
 
+  public getDecimals (networkAddress: string, decimals?: number): Promise<any> {
+    if (!this.utils.checkAddress(networkAddress)) {
+      return Promise.reject(`${networkAddress} is not a valid address.`)
+    }
+    return Promise.resolve(
+      decimals ||
+      // TODO replace with list of known currency network in clientlib
+      this.utils.fetchUrl(`networks/${networkAddress}`)
+        .then(network => network.decimals)
+    )
+  }
 }
