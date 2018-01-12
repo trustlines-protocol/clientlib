5ae32ddbe30c7358ae513ee3053d1562b1160a03
Bernd Bohmeier <berndbohmeier@googlemail.com>
2018-01-12 16:26:58 +0100

    Add notification for travis build
    Change travis build to run tests seperate

--- .travis/build_e2e_tests.sh
+++ .travis/build_e2e_tests.sh
@@ -4,8 +4,5 @@ PWD="$(pwd)"
 DIR="$(dirname "$(readlink -f "$0")")"
 
 cd $DIR/../tests/testrelay
-docker-compose up -d
-sleep 30
-npm run test:e2e
-docker-compose down
+docker-compose build
 cd "$PWD"
