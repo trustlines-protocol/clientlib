3e390f66828e4f3c46951f2e38ec8bf4df1e9a46
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-06-14 12:33:55 +0200

    change parseInt to BigNumber

--- src/Payment.ts
+++ src/Payment.ts
@@ -105,7 +105,7 @@ export class Payment {
     return this._transaction.prepValueTx(
       this._user.address,
       to,
-      parseInt(this._utils.calcRaw(value, 18), 10),
+      this._utils.calcRaw(value, 18),
       options
     )
   }
