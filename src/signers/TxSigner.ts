2ec4fd57709ea72fea6f2c3bcb5c1e87799b0bb7
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-07-30 11:58:24 +0200

    add keystore related methods to TxSigner interface

--- src/signers/TxSigner.ts
+++ src/signers/TxSigner.ts
@@ -1,12 +1,28 @@
 import {
   TxInfos,
-  RawTxObject
+  RawTxObject,
+  UserObject,
+  Signature,
+  Amount
 } from '../typings'
 
 /**
  * Interface for different signer strategies.
  */
 export interface TxSigner {
-  getTxInfos (userAddress: string): Promise<TxInfos>,
+  address: string
+  pubKey: string
+  keystore: any
+  createAccount (): Promise<UserObject>
+  loadAccount (serializedKeystore: string): Promise<UserObject>
+  signTx (rlpHexTx: string): Promise<string>
+  signMsgHash (msgHash: string): Promise<Signature>
+  getBalance (): Promise<Amount>
+  encrypt (msg: string, theirPubKey: string): Promise<any>
+  decrypt (encMsg: any, theirPubKey: string): Promise<any>
+  showSeed (): Promise<string>
+  recoverFromSeed (seed: string): Promise<UserObject>
+  exportPrivateKey (): Promise<string>
+  getTxInfos (userAddress: string): Promise<TxInfos>
   confirm (rawTx: RawTxObject)
 }
